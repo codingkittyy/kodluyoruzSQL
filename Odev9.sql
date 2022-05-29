1e30cd13c96117336b359d75c2ac2be358526780
tecosama <taylancankose@gmail.com>
2022-05-29 14:04:08 +0300

    orders-of-equalities-changed

--- Odev9.sql
+++ Odev9.sql
@@ -5,7 +5,7 @@ INNER JOIN city ON city.country_id = country.country_id;
 --customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name 
 --isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
 SELECT payment.payment_id, customer.first_name, customer.last_name from payment
-JOIN customer ON payment.customer_id = customer.customer_id;
+JOIN customer ON customer.customer_id = payment.customer_id;
 
 --customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name 
 --isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
